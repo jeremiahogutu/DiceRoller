ba3e97b5f924e5f9513de4009e6bcb8ab7eaba6b
Jeremiah Ogutu <jogutu@pm.me>
2020-01-12 16:49:01 -0500

    on click listener for button

--- app/src/main/java/com/hammerhead/diceroller/MainActivity.kt
+++ app/src/main/java/com/hammerhead/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hammerhead.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Lets Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
